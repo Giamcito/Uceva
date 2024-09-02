85880dbb2cd4ce75cb626d2b66dc95cd621a48d0
Camilo Giraldo <juan.giraldo03@uceva.edu.co>
2024-09-02 01:09:17 -0500

    Add files via upload

---
+++ celular-service/target/classes/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Apple', 'iPhone 15', 2000000, 128);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Samsung', 'Galaxy S23', 849, 256);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Xiaomi', 'Redmi Note 13', 699, 128);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Huawei', 'Nova 12s', 599, 128);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Microsoft', 'Lumia 650', 729, 256);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Sony', 'Xperia 1 V', 899, 512);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('OnePlus', 'OnePlus 11', 799, 12);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Google', 'Pixel 8', 699, 18);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Oppo', 'Find X6 Pro', 899, 20);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Vivo', 'Vivo X90', 749, 15);
+INSERT INTO celular (empresa, celular, precio, descuento) VALUES ('Realme', 'Realme GT 3', 649, 22);
